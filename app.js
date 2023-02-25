6c29994ae677adc126eb39141ac90cb94c4629fc
ruru-dev <rurutia89@gmail.com>
2023-02-24 21:31:18 -0600

    add functionality

---
+++ app.js
@@ -0,0 +1,14 @@
+const handleSubmit = function () {
+  let word = document.getElementById("word").value;
+  let wordLength = word.length;
+  let listWord = document.createElement("li");
+  listWord.innerText = word;
+
+  if (wordLength % 2 === 0) {
+    document.getElementById("even").appendChild(listWord);
+  } else if (wordLength % 2 === 1) {
+    document.getElementById("odd").appendChild(listWord);
+  } else {
+    console.log("please enter valid word");
+  }
+};
